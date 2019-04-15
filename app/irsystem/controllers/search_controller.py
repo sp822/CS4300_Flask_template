1cf269be8d84942d92b4b451e7de51607ddeeb03
Adeyemi A Oyemade <aao58@cornell.edu>
2019-04-14 20:01:38 -0400

    so progress

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -8,12 +8,11 @@ net_id = "sp822, sbz24, ky239, cne27, aao58"
 
 @irsystem.route('/', methods=['GET'])
 def search():
-	query = request.args.get('enjoyed')
-	if not query:
+	enjoyed = request.args.get('enjoyed')
+	if not enjoyed:
 		output = []
 		output_message = ''
 	else:
-		output_message = "Your search: " + query
-		output = range(5)
-		output = best_match(num_movies, movie_sims_cos, data, movie_index_to_name, movie_name_to_index, ["Doctor Stranger","Doctors","Emergency Couple"], [], ["Romance","Medical"], ["fun"], int(query))
+		output_message = "Your search: " + enjoyed
+		output = best_match(num_movies, movie_sims_cos, data, movie_index_to_name, movie_name_to_index, enjoyed, [], ["Romance","Medical"], ["fun"], 10)
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, output=output)

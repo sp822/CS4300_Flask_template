f9324576d5c501e97cdce505449d82823ab15410
sp822 <sp822@cornell.edu>
2019-04-16 10:10:59 -0400

    reduced time complexity by alot

--- app/irsystem/models/search.py
+++ app/irsystem/models/search.py
@@ -18,135 +18,13 @@ from collections import Counter
 import math
 #import scipy
 
-path = os.path.join(os.getcwd(), "app", "irsystem", "models", "Data-Set-Final.csv")
+path = os.path.join(os.getcwd(), "app", "irsystem", "models", "cleaneddata.csv")
 data = pd.read_csv(path)
-
-def cleanhtml(raw_html):
-    clean = re.compile('<.*?>')
-    cleantext = re.sub(clean, '', raw_html)
-    return cleantext
-
-def tokenize(text):
-    """Returns a list of words that make up the text.
-    Params: {text: String}
-    Returns: List
-    """
-    return list(filter(str.strip, list(map(lambda x: x, re.findall(r'[a-zA-Z]*', text)))))
-
-'''def stem(text):
-    stemmer=PorterStemmer()
-    stems = [stemmer.stem(w) for w in tokenize(text)]
-    return " ".join(stems)'''
-
-def preprocess_text(text):
-    text = str(text)
-    text = cleanhtml(text)
-    text = text.lower()
-    text = text.translate(str.maketrans('', '', string.punctuation))
-    text = text.strip()
-    return text
-
-def build_inverted_index_and_regular_index(data):
-    """ Builds an inverted index from the movies
-    """
-    result = {}
-    data_index = {}
-    for (index,value) in data['Summary'].items():
-        value = preprocess_text(value)
-        toks = value.split()
-        counts = Counter(toks)
-        data_index[index] = counts.items()
-        for word, count in counts.items():
-            if word not in result.keys():
-                result[word] = []
-            result[word].append((index, count))
-    return (result, data_index)
-
-def compute_idf(inv_idx, n_movies):
-    idf_dict = {}
-    for word in inv_idx.keys():
-        DF = len(inv_idx[word])
-        idf_dict[word] = math.log(n_movies/(1+DF),2)
-    return idf_dict
-
-def preprocess(data):
-    for (index,value) in data['Summary'].items():
-        value = preprocess_text(value)
-        data.loc[index,'Summary'] = value
-    return data
-
-def build_tfidf_matrix(data_index, n_movies,vocab_to_index, idf_dict):
-    result = np.zeros((n_movies, len(vocab_to_index.keys())))
-    for idx in range(n_movies):
-        sum_terms = 1
-        for (word, count) in data_index[idx]:
-            sum_terms = sum_terms + count
-            idx2 = vocab_to_index[word]
-            idf = idf_dict[word]
-            tfidf = idf*count
-            result[idx, idx2] = tfidf
-        result[idx,:]=np.divide(result[idx,:],sum_terms)
-
-    return result
-
-data = preprocess(data)
 num_movies = len(data)
-res = build_inverted_index_and_regular_index(data)
-inv_index = res[0]
-reg_index = res[1]
-idf_dict = compute_idf(inv_index, num_movies)
-index_to_vocab = {i:v for i, v in enumerate(inv_index.keys())}
-vocab_to_index = {v: k for k, v in index_to_vocab.items()}
-doc_by_vocab = build_tfidf_matrix(reg_index, num_movies,vocab_to_index, idf_dict)
+path2 = os.path.join(os.getcwd(), "app", "irsystem", "models", 'cosinematrix.npy')
+movie_sims_cos = np.load(path2)
 movie_index_to_name = data['Title'].to_dict()
 movie_name_to_index = {v: k for k, v in movie_index_to_name.items()}
-
-def get_sim(mov1, mov2, input_doc_mat, movie_name_to_index):
-    """Returns a float giving the cosine similarity of
-       the two movie transcripts.
-
-    Params: {mov1: String,
-             mov2: String,
-             input_doc_mat: Numpy Array,
-             movie_name_to_index: Dict}
-    Returns: Float (Cosine similarity of the two movie transcripts.)
-    """
-    idx1 = movie_name_to_index[mov1]
-    idx2 = movie_name_to_index[mov2]
-    movie1 = input_doc_mat[idx1,]
-    movie2 = input_doc_mat[idx2,]
-
-
-
-    #dot_product = 1 - scipy.spatial.distance.cosine(movie1, movie2)
-    dot_product = np.dot(movie1, movie2)
-    #/(np.linalg.norm(movie1)* np.linalg.norm(movie2))
-    return dot_product
-
-def build_movie_sims_cos(n_mov, movie_index_to_name, input_doc_mat, movie_name_to_index, input_get_sim_method):
-    """Returns a movie_sims matrix of size (num_movies,num_movies) where for (i,j):
-        [i,j] should be the cosine similarity between the movie with index i and the movie with index j
-
-    Params: {n_mov: Integer,
-             movie_index_to_name: Dict,
-             input_doc_mat: Numpy Array,
-             movie_name_to_index: Dict,
-             input_get_sim_method: Function}
-    Returns: Numpy Array
-    """
-    result = np.zeros((n_mov, n_mov))
-    for i in range(n_mov):
-        for j in range(n_mov):
-            if i == j:
-                result[i,j] = 0
-            else:
-                mov1 = movie_index_to_name[i]
-                mov2 = movie_index_to_name[j]
-                result[i,j] = input_get_sim_method(mov1, mov2, input_doc_mat, movie_name_to_index)
-    return result
-
-movie_sims_cos = build_movie_sims_cos(num_movies, movie_index_to_name, doc_by_vocab, movie_name_to_index, get_sim)
-
 # def display_sim_matrix(sim_matrix, diag = False):
 #     fig, ax = plt.subplots()
 #     plt_title = "KDramas Cos-Sim Heatmap"
@@ -181,6 +59,18 @@ movie_sims_cos = build_movie_sims_cos(num_movies, movie_index_to_name, doc_by_vo
 #              hist_kws={'edgecolor':'black'},
 #              kde_kws={'linewidth': 4})
 
+def cleanhtml(raw_html):
+    clean = re.compile('<.*?>')
+    cleantext = re.sub(clean, '', raw_html)
+    return cleantext
+
+def preprocess_text(text):
+    text = str(text)
+    text = cleanhtml(text)
+    text = text.lower()
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    text = text.strip()
+    return text
 
 def best_match(n_mov, movie_sims_cos, data, movie_index_to_name, movie_name_to_index, dramas_enjoyed, dramas_disliked, preferred_genres, preferred_network, num_results):
     feature_list = ['Summary_Similarity', 'Genre_Similarity', 'Network_Similarity', 'Total']

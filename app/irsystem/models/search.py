f14bd5f8e45fcf744bd58d2e3bcec9d285913a19
sp822 <sp822@cornell.edu>
2019-04-20 19:55:45 -0400

    changes

--- app/irsystem/models/search.py
+++ app/irsystem/models/search.py
@@ -1,68 +1,72 @@
 from __future__ import print_function
-# matplotlib.use('Agg')
-# import matplotlib.pyplot as plt
-#import requests
 import re
 import string
 from operator import itemgetter
-'''from nltk.stem import PorterStemmer'''
 import os
 import numpy as np
 import pandas as pd
 from collections import Counter
-#import csv
-#import matplotlib
-#matplotlib.use('TkAgg')
-#import matplotlib.pyplot as plt
-#import seaborn as sns
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 import math
-#import scipy
+import json
+from nltk.stem import PorterStemmer
 
-path = os.path.join(os.getcwd(), "app", "irsystem", "models", "cleaneddata.csv")
+
+path = os.path.join(os.getcwd(),"app", "irsystem", "models", "cleaned_comprehensive_data.csv")
 data = pd.read_csv(path)
-num_movies = len(data)
-path2 = os.path.join(os.getcwd(), "app", "irsystem", "models", 'cosinematrix.npy')
-movie_sims_cos = np.load(path2)
-movie_index_to_name = data['Title'].to_dict()
-movie_name_to_index = {v: k for k, v in movie_index_to_name.items()}
-# def display_sim_matrix(sim_matrix, diag = False):
-#     fig, ax = plt.subplots()
-#     plt_title = "KDramas Cos-Sim Heatmap"
-#     plt.title(plt_title, fontsize = 18)
-#     ttl = ax.title
-#     ttl.set_position([0.5, 1.05])
+num_dramas = len(data)
+path2 = os.path.join(os.getcwd(),"app", "irsystem", "models",'cosine_matrix.npy')
+drama_sims_cos = np.load(path2)
+path3 = os.path.join(os.getcwd(),"app", "irsystem", "models",'korean_data.csv')
+non_processed_data = pd.read_csv(path3)
+path4 = os.path.join(os.getcwd(),"app", "irsystem", "models",'genre_inclusion_matrix.npy')
+genre_inclusion_matrix  = np.load(path4)
+path5 = os.path.join(os.getcwd(),'actors_inclusion_matrix.npy')
+actors_inclusion_matrix  = np.load(path5)
+path6 = os.path.join(os.getcwd(),'"app", "irsystem", "models",years_inclusion_matrix.npy')
+years_inclusion_matrix  = np.load(path6)
+non_processed_data = pd.read_csv(path3)
+drama_index_to_name = non_processed_data['Title'].to_dict()
+process_dict = data['Title'].to_dict()
+drama_name_to_index = {v: k for k, v in process_dict.items()}
+drama_name_to_index_unprocess = {v: k for k, v in drama_index_to_name.items()}
 
-#     ax.set_xticklabels([])
-#     ax.set_yticklabels([])
-#     ax.axis('off')
-
-#     mask = None
-#     if diag:
-#         mask = np.tri(sim_matrix.shape[0], k=-1)
-
-#     heatmap = sns.heatmap(sim_matrix, fmt="", cmap='BuGn_r', linewidths=0, mask = mask, ax=ax)
-
-#     plt.show()
-#     fig = heatmap.get_figure()
-#     fig.savefig('sim_heatmap1.png', dpi=400)
-
-#     m_size = len(sim_matrix)
-#     scores = np.zeros((m_size+1)//2*m_size)
-#     cnt = 0
-#     for i in range (0, m_size):
-#         for j in range(i, m_size):
-#             scores[cnt] = sim_matrix[i][j]
-#             cnt+=1
-
-#     sns.distplot(scores, hist=True, kde=True,
-#              bins=int(180/5), color = 'darkblue',
-#              hist_kws={'edgecolor':'black'},
-#              kde_kws={'linewidth': 4})
+with open('genre_name_to_index.json') as fp:
+    genre_name_to_index = json.load(fp)
+with open('actors_name_to_index.json') as fp2:
+    actors_name_to_index = json.load(fp2)
+with open('years_name_to_index.json') as fp3:
+    years_name_to_index = json.load(fp3)
+with open('genres_dict.json') as fp:
+    genre_dict = json.load(fp)
+with open('actors_dict.json') as fp2:
+    actors_dict = json.load(fp2)
+with open('years_dict.json') as fp3:
+    years_dict = json.load(fp3)
+print(genre_dict)
 
 def cleanhtml(raw_html):
     clean = re.compile('<.*?>')
     cleantext = re.sub(clean, '', raw_html)
     return cleantext
+
+def tokenize(text):
+    """Returns a list of words that make up the text.
+    Params: {text: String}
+    Returns: List
+    """
+    return list(filter(str.strip, list(map(lambda x: x, re.findall(r'[a-zA-Z]*', text)))))
+
+def stem(text):
+    """Removes stems from a string.
+    Params: {text: String}
+    Returns: String
+    """
+    stemmer=PorterStemmer()
+    stems = [stemmer.stem(w) for w in tokenize(text)]
+    return " ".join(stems)
 
 def preprocess_text(text):
     text = str(text)
@@ -70,54 +74,90 @@ def preprocess_text(text):
     text = text.lower()
     text = text.translate(str.maketrans('', '', string.punctuation))
     text = text.strip()
+    text = stem(text)
     return text
 
-def best_match(n_mov, movie_sims_cos, data, movie_index_to_name, movie_name_to_index, dramas_enjoyed, dramas_disliked, preferred_genres, preferred_network, num_results):
-    feature_list = ['Summary_Similarity', 'Genre_Similarity', 'Network_Similarity', 'Total']
-    result = pd.DataFrame(0, index=np.arange(n_mov), columns=feature_list)
+def map_network(network,x):
+    if x == network:
+        return 1
+    else:
+        return 0
+
+def best_match(actors_dict, genre_inclusion_matrix, actors_inclusion_matrix, years_inclusion_matrix, genre_name_to_index, actors_name_to_index, years_name_to_index, drama_sims_cos, data, drama_index_to_name, drama_name_to_index, dramas_enjoyed, dramas_disliked, preferred_genres, preferred_network, preferred_actors, preferred_time_frame, num_results):
+
+    feature_list = ['Summary_Similarity', 'Actor_Similarity', 'Genre_Similarity', 'Network_Similarity','Year_Similarity', 'Total']
+    result = pd.DataFrame(0, index=np.arange(1466), columns=feature_list)
     genres = set()
     preferred_genres = [preprocess_text(value) for value in preferred_genres]
+    print(preferred_genres)
     genres.update(preferred_genres)
+    print(genres)
+    years = preferred_time_frame.split('-')
+    start_year = int(years[0])
+    end_year = int(years[1])
+    preferred_actors = preferred_actors.split(", ")
+    preferred_actors_set = set()
+    preferred_actors_set.update(preferred_actors)
+    d = {k:len(v) for k, v in actors_dict.items()}
+    actors_len_df = pd.DataFrame.from_dict(d, orient='index')
+    actors_len_df.columns = ['Length']
     for drama in dramas_enjoyed:
-        if drama in movie_name_to_index.keys():
-            index = movie_name_to_index[drama]
-            sim = movie_sims_cos[index,:]
+        drama = drama.lower()
+        drama = drama.strip()
+        if drama in drama_name_to_index.keys():
+            index = drama_name_to_index[drama]
+            sim = drama_sims_cos[index,:1466]
             result['Summary_Similarity']+= pd.Series(sim)
-
     for drama in dramas_disliked:
-        if drama in movie_name_to_index.keys():
-            index = movie_name_to_index[drama]
-            sim = movie_sims_cos[index,:]
+        drama = drama.lower()
+        drama = drama.strip()
+        if drama in drama_name_to_index.keys():
+            index = drama_name_to_index[drama]
+            sim = drama_sims_cos[index,:1466]
             result['Summary_Similarity']-= pd.Series(sim)
-
-    for index, value in data.iterrows():
-        gen = str(value['Genre'])
-        gen = preprocess_text(gen)
-        drama_genres = set()
-        drama_genres.update(gen.split())
-        result.loc[index,'Genre_Similarity'] = len(genres.intersection(drama_genres))/len(genres.union(drama_genres))
-        if preferred_network == data.iloc[index]['Network']:
-            result['Network_Similarity']+=1
-    result['Total'] = result.sum(axis = 1)
-
+    for genre in preferred_genres:
+        if genre in genre_name_to_index.keys():
+            index = genre_name_to_index[genre]
+            result['Genre_Similarity']= genre_inclusion_matrix[:,index]
+    for actor in preferred_actors:
+        if actor in actors_name_to_index.keys():
+            index = actors_name_to_index[actor]
+            result['Actor_Similarity']+= actors_inclusion_matrix[:,index]
+    actors_len_df['Length'] =  actors_len_df['Length'] + len(preferred_actors)
+    actors_len_df['Length'] = actors_len_df['Length'].subtract(result['Actor_Similarity'], fill_value = 0)
+    actor_sim2=result['Actor_Similarity']
+    for idx in range(1466):
+        result['Actor_Similarity'] = actor_sim2.iloc[idx]/actors_len_df['Length'].iloc[idx]
+    if str(start_year) in years_name_to_index.keys():
+        index = years_name_to_index[str(start_year)]
+        result['Year_Similarity'] = years_inclusion_matrix[:,index]
+    if str(end_year) in years_name_to_index.keys():
+        index = years_name_to_index[str(end_year)]
+        result['Year_Similarity'] = pd.concat([pd.Series(years_inclusion_matrix[:,index]), result['Year_Similarity']], axis=1).min(axis=1)
+    result['Network_Similarity'] = data['Network'].apply(lambda x: map_network(x, preferred_network))
+    result['Year_Similarity'] = 1 - result['Year_Similarity']/(result['Year_Similarity'].max()+1)
+    result['Total'] = round(result['Summary_Similarity']*.6 + result['Actor_Similarity']*.1 + result['Year_Similarity']*.05 + result['Genre_Similarity']*.2 + result['Network_Similarity']*.05,5)
     result = result.sort_values(by='Total', ascending=False)
     result = result[:num_results]
     indices =  result.index.tolist()
-    best_dramas = pd.Series([movie_index_to_name[index] for index in indices],index = result.index)
+    best_dramas = pd.Series([drama_index_to_name[index] for index in indices],index = result.index)
     result.insert(loc=0, column='Drama_Title', value=best_dramas)
     result.reset_index()
     return result
 
-def display (n_mov, movie_sims_cos, data, movie_index_to_name, movie_name_to_index, dramas_enjoyed, dramas_disliked, preferred_genres, preferred_network, num_results):
+def display (dramas_enjoyed, dramas_disliked, preferred_genres, preferred_network, preferred_actors, preferred_time_frame, num_results):
     dramas_enjoyed = dramas_enjoyed.split(', ')
     dramas_disliked = dramas_disliked.split(', ')
     preferred_genres = preferred_genres.split(', ')
-    preferred_network = preferred_network.split(', ')
-    best = best_match(n_mov, movie_sims_cos, data, movie_index_to_name, movie_name_to_index, dramas_enjoyed, dramas_disliked, preferred_genres, preferred_network, num_results)
+    best = best_match(actors_dict, genre_inclusion_matrix, actors_inclusion_matrix, years_inclusion_matrix, genre_name_to_index, actors_name_to_index, years_name_to_index,drama_sims_cos, data, drama_index_to_name, drama_name_to_index,  dramas_enjoyed, dramas_disliked, preferred_genres, preferred_network, preferred_actors, preferred_time_frame, num_results)
     title = list(zip(best['Drama_Title'], best["Total"]))
     final = {}
-    for x in title:
+    for idx, x in enumerate(title):
         title_name = x[0]
         final.update({x[0]: ''})
-        final[title_name] += data['Summary'][list(data['Title']).index(title_name)]
+        summary = str(non_processed_data['Summary'].loc[drama_name_to_index_unprocess[title_name]])
+        if summary != "nan":
+            final[title_name] += summary
     return ['Drama Title: {},  Summary: {},  Total Similarity Score: {}'.format(x[0], final[x[0]], x[1]) for x in title]
+
+print(display("House, The Mindy Project", "The Office, Game of Thrones", "medical", "", "","2010-2015", 10))

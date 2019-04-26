e763d5b7f5c4cac8b3cb8787c97d1d10a6de7e77
sp822 <sp822@cornell.edu>
2019-04-26 15:09:19 -0400

    Revert "removed preferred network"
    This reverts commit 07ef0c614bc6f4d6da0b5b6498fd708c8e177496.

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -29,22 +29,25 @@ def search():
 		preferred_time_frame.append(preferred_from)
 		preferred_time_frame.append(preferred_to)
 	else:
-		preferred_time_frame.append("1958")
+		preferred_time_frame.append("1938")
 		preferred_time_frame.append("2019")
 
+	preferred_networks = request.args.get("preferred_networks")
 	preferred_actors = request.args.get('preferred_actors')
 	clicked_img = request.args.get("img-click")
 	num_results = 9
 
 	
-	if not dramas_enjoyed and not dramas_disliked and not preferred_genres and not preferred_actors:
+	if not dramas_enjoyed and not dramas_disliked and not preferred_genres and not preferred_actors and not preferred_networks:
 		output = []
 		output_message = ''
 		return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, genre=genre_list, titles = titles_list, clicked_img = clicked_img, networks = network_list, output=output)
 	else:
-		output_message = "So you like {}, dislike {}, like the Genre(s) {} and actor(s) {}, from {}, to {}.". format(dramas_enjoyed, dramas_disliked, preferred_genres, preferred_actors, preferred_time_frame[0], preferred_time_frame[1],)
-		
-		output = display (dramas_enjoyed, dramas_disliked, preferred_genres, preferred_actors, preferred_time_frame, num_results)
+		if preferred_networks == None:
+			output_message = "So you like {}, dislike {}, like the Genre(s) {} and actor(s) {}, from {}, to {}.". format(dramas_enjoyed, dramas_disliked, preferred_genres, preferred_actors, preferred_time_frame[0], preferred_time_frame[1],)
+		else:
+			output_message = "So you like {}, dislike {}, like the Genre(s) {} and actor(s) {}, from {}, to {}, with this network {}.".format(dramas_enjoyed, dramas_disliked, preferred_genres, preferred_actors, preferred_time_frame[0], preferred_time_frame[1], preferred_networks)			
+		output = display (dramas_enjoyed, dramas_disliked, preferred_genres, preferred_networks, preferred_actors, preferred_time_frame, num_results)
 		return render_template('results.html', name=project_name, netid=net_id, output_message=output_message, genre=genre_list, titles = titles_list, clicked_img = clicked_img, networks = network_list, output=output)
 	if request.args.get('new-search'):
 		return  render_template('search.html', name=project_name, netid=net_id, output_message=output_message, genre=genre_list, titles = titles_list,  clicked_img = clicked_img, networks = network_list, output=output)
